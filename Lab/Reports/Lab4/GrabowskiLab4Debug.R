a016269376827f4eae0fec7218f830aa78646b20
Jonathan Grabowski <johngrab7@gmail.com>
2022-10-09 20:26:58 +0000

    Added two new comments (lines 11 and 21) to make it more clear what is happening with the code

--- Lab/Reports/Lab4/GrabowskiLab4Debug.R
+++ Lab/Reports/Lab4/GrabowskiLab4Debug.R
@@ -8,6 +8,7 @@ Lab4Data$Paper.ID <- gsub("\\'", "", Lab4Data$Paper.ID)
 Lab4Data$Paper.ID <- gsub(" ", "", Lab4Data$Paper.ID)
 
 # We made a loop embedded in a loop that removes the junk papers from the Paper.ID column of the dataset Lab4Data. 
+# In this example, junk papers are considered to be papers that show up exactly one time.
 for (each in c(length(Lab4Data$Paper.ID):1)){
   if (length(unique(unlist(strsplit(Lab4Data$Paper.ID[each], ",")))) == 1){
     Lab4Data <- Lab4Data[-c(each),]
@@ -17,7 +18,7 @@ for (each in c(length(Lab4Data$Paper.ID):1)){
 #This line of code will print the number rows containing non-junk Paper IDs.
 print(length(Lab4Data$Paper.ID))
 
-
+# There are 83 papers that are not junk in the dataset
 
 
 
